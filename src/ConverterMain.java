5a193483a82dd87bf9659adf9dda783d635614c3
aswad <aswad.m.tariq13@gmail.com>
2025-10-05 12:58:38 +0800

    basic error handling

--- src/ConverterMain.java
+++ src/ConverterMain.java
@@ -2,18 +2,27 @@ import java.util.Scanner;
 
 public class ConverterMain {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        Scanner in = null;
 
-        System.out.println("Enter your string");
-        StringBuilder sb = new StringBuilder();
-
-        while (in.hasNextLine()) {
-            String input = in.nextLine().toLowerCase();
-
-            sb.append(input);
-            sb.append("\n");
-
-            System.out.println("Result: " + sb);
+        try {
+            in = new Scanner(System.in);
+            System.out.println("Enter your string");
+            StringBuilder sb = new StringBuilder();
+    
+            while (in.hasNextLine()) {
+                String input = in.nextLine().toLowerCase();
+    
+                sb.append(input);
+                sb.append("\n");
+    
+                System.out.println("Result: " + sb);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (in != null) {
+                in.close();
+            }
         }
     }
 }
